e2b3bde4d1c39d8ba4053b619a370c0e258f89af
Charly Rios <juan.rios@teachable.com>
2022-03-06 15:50:04 -0600

    Backend user sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        #debugger
+        if @user.save
+            puts "Saved"
+            flash[:notice] = "Welcome to the page"
+            redirect_to articles_path
+        else
+            render :action => :new
+        end
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
