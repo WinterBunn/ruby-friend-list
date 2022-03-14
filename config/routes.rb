f0b9f1aad0e37ed937eb3266b18999e6d3ae5d7f
Charly Rios <juan.rios@teachable.com>
2022-03-14 16:51:24 -0600

    Users session

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, except: [:new] 
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
