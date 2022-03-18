d3b1fb661a5451894cdb25946a1146c0077ede92
Charly Rios <juan.rios@teachable.com>
2022-03-18 16:20:15 -0600

    Restrict actions in controller level

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
     def show
         #params is where all the parameters are allocated
         #in order to make the variable available in all the app yo need to make it an instance with "@"
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -25,11 +28,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was created successfully"
             #How this redirect works?
@@ -40,13 +41,24 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
 
+    private
     # Only allow a list of trusted parameters through.
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
+      end
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
     end
 end
