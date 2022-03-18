a45e8fb7fdb1c04ea0714f187d997f2984efbc40
Charly Rios <juan.rios@teachable.com>
2022-03-17 19:18:07 -0600

    Restrict actions for logged in users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,3 @@
 module ApplicationHelper
 
-    def current_user
-      #if we have @current_user, we return @current_user, if not we look for it and save it in current user (||=)
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
-    
-    def logged_in?
-      #makes current user into a boolean
-      !!current_user
-    end
 end
